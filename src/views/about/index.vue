b972c016526df955e7aab0882fb46224f405f3d8
guozheng <guozheng@fangdd.com>
2017-03-16 00:01:46 +0800

    about

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -8,72 +8,71 @@
                 <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
             </div>
         </div>
+
+        <hr class="cut-off-divider">
+
         <!--content-->
-        <div class="container">
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
+                        <h2>原版国内镜像</h2>
+                        <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                        <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
+                        <h2>原版国内镜像</h2>
+                        <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                        <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
+                        <h2>原版国内镜像</h2>
+                        <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                        <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                    </div>
+                </div>
 
-            <hr class="cut-off-divider">
+                <hr class="cut-off-divider">
 
-            <div class="row">
-                <div class="col-md-4">
-                    <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
-                    <h2>原版国内镜像</h2>
-                    <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                    <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                <div class="row">
+                    <div class="col-md-7">
+                        <h2>First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+                        <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
-                    <h2>原版国内镜像</h2>
-                    <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                    <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+
+                <hr class="cut-off-divider">
+
+                <div class="row">
+                    <div class="col-md-7 col-md-push-5">
+                        <h2>Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+                        <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                    </div>
+                    <div class="col-md-5 col-md-pull-7">
+                        <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
-                    <h2>原版国内镜像</h2>
-                    <p>最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                    <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+
+                <hr class="cut-off-divider">
+
+                <div class="row">
+                    <div class="col-md-7">
+                        <h2>原版国内镜像 <span class="text-muted">Checkmate.</span></h2>
+                        <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="500x500" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
+                    </div>
                 </div>
+
             </div>
-
-            <hr class="cut-off-divider">
-
-            <div class="row">
-                <div class="col-md-7">
-                    <h2>First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                    <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                </div>
-                <div class="col-md-5">
-                    <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
-                </div>
-            </div>
-
-            <hr class="cut-off-divider">
-
-            <div class="row">
-                <div class="col-md-7 col-md-push-5">
-                    <h2>Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                </div>
-                <div class="col-md-5 col-md-pull-7">
-                    <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
-                </div>
-            </div>
-
-            <hr class="cut-off-divider">
-
-            <div class="row">
-                <div class="col-md-7">
-                    <h2>原版国内镜像 <span class="text-muted">Checkmate.</span></h2>
-                    <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
-                </div>
-                <div class="col-md-5">
-                    <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="500x500" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
-                </div>
-            </div>
-
-            <hr class="cut-off-divider">
-
-        </div>
-
+        </section>
     </div>
 </template>
 
--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -1,60 +1,32 @@
 <template>
     <div class="template-wrapper">
 
-        <!--carousel-->
-        <section>
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img class="first-slide" src="/static/images/solution.jpg" alt="First slide">
-
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Example headline.</h1>
-
-                                <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous"
-                                    Glyphicon buttons on the left and right might not load/display properly due to web browser
-                                    security rules.</p>
-
-                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img class="second-slide" src="/static/images/b1.jpg" alt="Second slide">
-
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Another example headline.</h1>
-
-                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida
-                                    at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-                                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+        <div class="jumbotron" style="background:#d9edf7;">
+            <div class="container">
+                <h1>原版国内镜像</h1>
+                <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+                <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
             </div>
-        </section>
+        </div>
+
+
+        <hr class="cut-off-divider">
 
         <!--content-->
-        <hr class="cut-off-divider">
         <section>
             <div class="container">
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-4" :key="index" v-for="(item, index) in record">
+                        <img class="img-responsive" src="/static/images/placeholder_400x250.svg" alt="Generic placeholder image" width="400" height="250">
+                        <h2>{{ item.title }}</h2>
+                        <p>{{ item.content }}</p>
+                        <p><a class="btn btn-default" href="#" role="button">查看详情 »</a></p>
+                    </div>
+                </div>
+
+                <hr class="cut-off-divider">
+
                 <div class="row">
                     <div class="col-md-7">
                         <h2>First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
@@ -88,7 +60,6 @@
                         <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="500x500" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
                     </div>
                 </div>
-
             </div>
         </section>
 
@@ -98,19 +69,24 @@
 
 <script>
 
+
 export default {
-    name: 'index',
+    name: 'product',
     data () {
         return {
+            record: {}
         }
     },
     components: {
     },
     created () {
-        console.log("created index")
+        console.log("created product")
+        this.$store.dispatch('getProduct', {}).then((res) => {
+            this.record = {...this.record, ...res}
+        })
     },
     mounted () {
-        console.log("mounted index")
+        console.log("mounted product")
     },
     methods:{
 
