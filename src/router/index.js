ba834b6e130dcbf9594a2af08f3631ac422dbfdf
zhengzheng <zhengzheng@juanpi.com>
2017-03-16 16:34:42 +0800

    wow

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import About from '@/views/about/'
 // import Service from '@/views/service/'
 import Product from '@/views/product/'
 import Contact from '@/views/contact/'
-// import Questions from '@/views/questions/'
+import Questions from '@/views/questions/'
 // import Solution from '@/views/solution/'
 
 Vue.use(VueRouter)
@@ -36,7 +36,7 @@ const router = new VueRouter({
     { path: '/about', name: 'about', component: About },
     //{ path: '/service', name: 'service', component: Service },
     { path: '/product', name: 'product', component: Product },
-    //{ path: '/questions', name: 'questions', component: Questions },
+    { path: '/questions', name: 'questions', component: Questions },
     //{ path: '/solution', name: 'solution', component: Solution },
     { path: '/contact', name: 'contact', component: Contact }
   ]
--- src/views/questions/index.vue
+++ src/views/questions/index.vue
@@ -40,11 +40,11 @@
                 <hr class="cut-off-divider">
 
                 <div class="row">
-                    <div class="col-md-7">
+                    <div class="col-md-7 wow slideInLeft">
                         <h2>First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
                         <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-5 wow slideInRight">
                         <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
                     </div>
                 </div>
@@ -52,11 +52,11 @@
                 <hr class="cut-off-divider">
 
                 <div class="row">
-                    <div class="col-md-7 col-md-push-5">
+                    <div class="col-md-7 col-md-push-5 wow slideInRight">
                         <h2>Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
                         <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
                     </div>
-                    <div class="col-md-5 col-md-pull-7">
+                    <div class="col-md-5 col-md-pull-7 wow slideInLeft">
                         <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="400x250" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
                     </div>
                 </div>
@@ -64,11 +64,11 @@
                 <hr class="cut-off-divider">
 
                 <div class="row">
-                    <div class="col-md-7">
+                    <div class="col-md-7 wow slideInLeft">
                         <h2>原版国内镜像 <span class="text-muted">Checkmate.</span></h2>
                         <p class="lead">最受欢迎的前端技术教程网站，但是国内用户一直不能访问，并且国内的中文翻译版本十分陈旧。因此做了个镜像，希望英文好的同学直接去看原版教程吧</p>
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-5 wow slideInRight">
                         <img class="img-responsive center-block" data-src="/static/images/placeholder_400x250.svg" alt="500x500" src="/static/images/placeholder_400x250.svg" data-holder-rendered="true">
                     </div>
                 </div>
@@ -92,6 +92,7 @@ export default {
     },
     created () {
         console.log("created questions")
+        new WOW().init();
     },
     mounted () {
         console.log("mounted questions")
